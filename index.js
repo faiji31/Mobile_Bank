cba18a550226b4d7dfb5053ca0dc7e297fdb3e1b
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-25 19:00:11 +0600

    6th commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 document.getElementById("loginbtn").addEventListener('click',function(e){
     e.preventDefault()
+    const mobilenumber = 17905289111;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobilenumberValueConverted = parseInt(mobileNumberValue);
+    const pinNumbervalue = document.getElementById("pin-number").value
+    const PinNumbervalueConverted = parseInt(pinNumbervalue);
+
+    console.log(mobilenumberValueConverted,PinNumbervalueConverted);
+
+
+    if(mobilenumberValueConverted === mobilenumber && PinNumbervalueConverted === pinNumber){
+        console.log("all value matched");
+
+    }else{
+        console.log('invalid');
+    }
     
 
 })
