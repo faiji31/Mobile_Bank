33be3b97415f2e810f598bfd100b4fffe1347511
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-30 00:35:26 +0600

    3rd commit

---
+++ home.js
@@ -0,0 +1,28 @@
+const validPin =1234
+document.getElementById('add-money-btn').addEventListener('click',function(e){
+    e.preventDefault()
+    const bank = document.getElementById('bank').value
+    const AccountNumber = document.getElementById('bank-acc').value 
+    if (AccountNumber.length<11){
+    alert("Please Enter your Valid Acc Number")
+    return;
+   }
+
+    const AmountAdd = parseInt(document.getElementById('amount-add').value)
+    
+    const pin = parseInt(document.getElementById('pin-number').value)
+
+    if (pin!=1234){
+        alert("Enter Your Valid Pin")
+        return
+    }
+   const AvilableBalance = parseInt(document.getElementById('money').innerHTML)
+
+   
+   
+
+
+
+   const totalBalance = AmountAdd + AvilableBalance
+   document.getElementById('money').innerHTML = totalBalance
+})
