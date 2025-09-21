de7a3f800658c320226d38f97bd4051519480572
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-22 01:38:52 +0600

    21th commit

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,39 @@
+const validpin =1234
+
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
    
     const bank = document.getElementById('bank').value
     const Accountnumber = document.getElementById('account-number').value
     const Addmoney = parseInt( document.getElementById('add-money').value)
-    const Pin = document.getElementById('pin-number').value
+    const Pin = parseInt(document.getElementById('pin-number').value)
 
     const AvilableBalence = parseInt(document.getElementById('tk').innerText)
-    console.log(AvilableBalence)
+   if(Accountnumber.length<11){
+    alert("enter your valid account number")
+    return
+   }
+
+   if (Pin!==validpin){
+    alert("enter your valid pin")
+    return
+   }
 
     const totalnewAilableBalence = Addmoney + AvilableBalence
     document.getElementById('tk').innerText =totalnewAilableBalence
    
+})
+
+// toggoling feature
+document.getElementById('add-btn').addEventListener('click',function(e){
+    e.preventDefault()
+    document.getElementById('cash-out').style.display = 'none'
+     document.getElementById('add-money').style.display = 'block'
+
+})
+document.getElementById('cash-btn').addEventListener('click',function(e){
+    e.preventDefault()
+    document.getElementById('add-money').style.display = 'none'
+     document.getElementById('cash-out').style.display = 'block'
+
 })
