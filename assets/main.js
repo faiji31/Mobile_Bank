6f9812a83ac7e121005b497b5852515eef3a578d
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-22 00:22:27 +0600

    17th commit

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,9 @@
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
-    console.log('click')
+   
+    const bank = document.getElementById('bank').value
+    const Accountnumber = document.getElementById('account-number').value
+    const Addmoney = parseInt( document.getElementById('add-money').value)
+    const Pin = document.getElementById('pin-number').value
+    console.log(bank,Accountnumber,Addmoney,Pin)
 })
